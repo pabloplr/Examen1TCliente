a1d18cb9eb3cda475c3621016dc808b726e2118f
pabloplr <47282256+pabloplr@users.noreply.github.com>
2021-12-02 12:21:59 +0100

    update product title

--- js/colections.js
+++ js/colections.js
@@ -110,7 +110,7 @@ function createProduct(name, price, url) {
                 <img src="${url}" alt=" ">
                 <div class="product_info">
                     <div class="data">
-                        <h3>${name}</h3>
+                        <h2>${name}</h2>
                         <p>${price} €</p>
                     </div>
                     <button onclick="addToCart('${name}','${price}')">añadir</button>
